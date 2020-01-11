a176b20268b2cffb9842ccaf9385142c5b634139
Brian Henry <BrianHenryIE@gmail.com>
2020-01-11 14:03:19 -0800

    Removed add_widget method
    Which is not from WPPB default.

--- src/interface-wppb-loader.php
+++ src/interface-wppb-loader.php
@@ -8,13 +8,6 @@ interface WPPB_Loader_Interface {
 
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 );
 
-	/**
-	 * @see register_widget()
-	 *
-	 * @param string|\WP_Widget $widget Either the name of a `WP_Widget` subclass or an instance of a `WP_Widget` subclass.
-	 */
-	public function add_widget( $widget );
-
 	public function run();
 
 }
